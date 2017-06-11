5b79ca893a5e42be85228ed787031a3096c610a3
Arkadiusz Plichta <el.pikel@gmail.com>
2017-06-11 12:39:40 +0200

    init

---
+++ lib/trade.ex
@@ -0,0 +1,18 @@
+defmodule Trade do
+  @moduledoc """
+  Documentation for Trade.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trade.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trade_test.exs
@@ -0,0 +1,8 @@
+defmodule TradeTest do
+  use ExUnit.Case
+  doctest Trade
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
