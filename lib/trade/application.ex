f6af529a79c2b32b8dfd6d3c06b1879aa90c4231
Arkadiusz Plichta <el.pikel@gmail.com>
2017-06-11 12:48:29 +0200

    supervised

---
+++ lib/trade/application.ex
@@ -0,0 +1,22 @@
+defmodule Trade.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Trade.Worker.start_link(arg1, arg2, arg3)
+      # worker(Trade.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Trade.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
